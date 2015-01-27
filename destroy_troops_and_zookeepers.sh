edb16676aadd9ddf3d61a8e420929eba84175818
Christoph Dwertmann <cdwertmann@gmail.com>
2015-01-27 17:29:46 +1100

    Add next script

---
+++ destroy_troops_and_zookeepers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+OS_TENANT_NAME="jns_monkeys"
+SSH="ssh $PM sudo"
+
+$SSH /etc/init.d/apache2 stop
+for i in $(seq $START_ID $STOP_ID); do 
+  nova delete zookeeper$i;
+  nova delete troop$i;
+  $SSH puppet cert clean troop$i.vaultsystems
+  $SSH puppet cert clean zookeeper$i.vaultsystems
+done
+$SSH rm /var/lib/puppet/ssl/ca/requests/* /var/log/puppet/*
+$SSH timeout 5 puppet master --no-daemonize --verbose
+$SSH /etc/init.d/apache2 start
+
+nova list | grep ERROR
+if [[ $? == 0 ]]; then exit 1; fi
