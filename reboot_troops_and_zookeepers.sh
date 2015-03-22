84cc1859e7b159b596b223e019e6d9bed0a24e0a
Christoph Dwertmann <cdwertmann@gmail.com>
2015-03-23 10:37:13 +1100

    faster reboots

--- reboot_troops_and_zookeepers.sh
+++ reboot_troops_and_zookeepers.sh
@@ -4,9 +4,5 @@ OS_TENANT_NAME="jns_monkeys"
 for i in $(seq $START_ID $STOP_ID); do 
 nova reboot troop$i;
 sleep $TROOP_INTERVAL;
-done
-
-for i in $(seq $START_ID $STOP_ID); do
 nova reboot zookeeper$i;
-sleep $ZK_INTERVAL;
 done
