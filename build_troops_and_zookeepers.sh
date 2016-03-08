52e7e9b5460ca406aea63565badff57eb4725a0b
Christoph Dwertmann <cdwertmann@gmail.com>
2016-03-08 16:31:11 +1100

    set AZ

--- build_troops_and_zookeepers.sh
+++ build_troops_and_zookeepers.sh
@@ -5,9 +5,9 @@ ZK_IMAGE=`glance image-list | grep "puppetclient" | head -1 | awk '{ print $2}'`
 NET=d9e502bb-b6ca-4a5c-8eb6-3e3473bf7082
 
 for i in $(seq $START_ID $STOP_ID); do
-nova boot --flavor m1.fortigate --block-device source=image,id=$ZK_IMAGE,dest=volume,size=3,shutdown=remove,bootindex=0 --nic net-id=$NET --key-name monkey_pm zookeeper$i;
+nova boot --flavor m1.fortigate --availability-zone nova --block-device source=image,id=$ZK_IMAGE,dest=volume,size=3,shutdown=remove,bootindex=0 --nic net-id=$NET --key-name monkey_pm zookeeper$i;
 sleep $ZK_INTERVAL;
-nova boot --flavor m1.large --block-device source=image,id=$TROOP_IMAGE,dest=volume,size=25,shutdown=remove,bootindex=0 --nic net-id=$NET troop$i;
+nova boot --flavor m1.large --availability-zone nova --block-device source=image,id=$TROOP_IMAGE,dest=volume,size=25,shutdown=remove,bootindex=0 --nic net-id=$NET troop$i;
 sleep $TROOP_INTERVAL;
 done
 
